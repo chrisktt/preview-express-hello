c7f27195a3a00b3110de408a15174fdbfc8d22a5
Chris Kirksey <chris.kirksey@aesbus.com>
2021-07-22 10:48:26 -0500

    Bump version to trigger build

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.send(
-  "Hello there! <br> V 101" +
+  "Hello there! <br> V 102" +
   "<br>hash = " + 
   (process.env.RENDER_GIT_COMMIT || "").substring(0,7) +
   ""
-  ));
+));
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port} !`));
