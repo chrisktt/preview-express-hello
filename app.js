d80fb3727d8531a095f10c4391c81aa2988d466e
Chris Kirksey <chris.kirksey@aesbus.com>
2021-07-26 14:47:36 -0500

    Change formatting

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send(
-  "Hello there! <br> V 102" +
-  "<br>hash = " + 
-  (process.env.RENDER_GIT_COMMIT || "").substring(0,7) +
-  ""
-));
+app.get('/', (req, res) =>
+    res.send(
+        'Hello there! <br> V 102' +
+            '<br>hash = ' +
+            (process.env.RENDER_GIT_COMMIT || '').substring(0, 7) +
+            ''
+    )
+);
 
-app.get("/status", (req, res) => res.status(200).json({message: 'ok'}))
+app.get('/status', (req, res) => res.status(200).json({ message: 'ok' }));
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port} !`));
-
