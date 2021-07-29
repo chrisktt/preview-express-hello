ba612f377ee62e5818f5193fa0b1b4dfc1b5dfad
Chris Kirksey <chris.kirksey@aesbus.com>
2021-07-29 10:55:12 -0500

    Start building a testing process

--- tools/kv-to-yaml.pl
+++ tools/kv-to-yaml.pl
@@ -1,7 +1,13 @@
 #!/usr/bin/env perl
 
 =pod
-Convert key-value pairs to YAML.
+Convert- key-value pairs to YAML.
+
+Usage:
+    kv-to-yaml.pl --test
+    will run tests
+
+    kv-to-yaml.pl < file.env > file.yaml
 
 For example:
    KEY=VALUE
@@ -10,7 +16,7 @@ becomes
      value: "VALUE"
 =cut
 
-sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
+if ("--test" eq $ARGV[0]) {test(); exit 0}; # Run tests if --test
 
 while (<>) {
     chomp;
@@ -21,3 +27,33 @@ while (<>) {
     print "  value: ", trim($val), "\n";
 }
 
+
+sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
+
+# Test code results
+sub test {
+$given = <<'END-OF-DATA';
+# My database parameters for surveys
+DB_NAME=surveys-test  # this is the test database
+DB_USER=alice
+DB_PASS=mypassword #secret
+DB_SERVER=surveys.aesbus.com:51234
+END-OF-DATA
+
+$expected = <<'END-OF-DATA';
+- key: DB_NAME
+  value: surveys-test  # this is the test database
+- key: DB_USER
+  value: alice
+- key: DB_PASS
+  value: mypassword #secret
+- key: DB_SERVER
+  value: surveys.aesbus.com:51234
+END-OF-DATA
+
+
+# Send data to this script (currently kv-to-yaml.pl)
+open(TEST, "| perl $0") or die "cannot create pipe: $!";
+print TEST $given;
+close(TEST);
+}
