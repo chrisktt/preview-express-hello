a8ee4fe3cf3da26a19fa9b522379b87a8a3931e2
Chris Kirksey <chris.kirksey@aesbus.com>
2021-08-03 15:19:39 -0500

    Demonstrate how to use database module

---
+++ exp.js
@@ -0,0 +1,20 @@
+const config = require('config'); // https://github.com/lorenwest/node-config/wiki
+const dbConfig = config.get('database');
+
+const NODE_ENV = config.util.getEnv('NODE_ENV');
+console.log(`NODE_ENV = ${NODE_ENV}`);
+
+const database = require('./database.js');
+database
+    .connectToDatabase()
+    .then((db) => {
+        console.log(`GOT IT Connected to database: ${dbConfig.name}`);
+        let dbStatus = db.connection.readyState;
+        console.log(`dbStatus = ${dbStatus}`);
+        // start();
+        process.exit();
+    })
+    .catch((err) => {
+        console.log('Failed to connect to DB: ', err);
+        process.exit();
+    });
