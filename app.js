9be3877945230347d8d57931b828dadb8736cc7d
Chris Kirksey <chris.kirksey@aesbus.com>
2021-08-05 13:29:11 -0500

    Simplify app.js

--- app.js
+++ app.js
@@ -38,26 +38,17 @@ app.get('/', (req, res) =>
 );
 
 app.get('/app_status', (req, res) => {
-    // http://expressjs.com/en/api.html#res.status
     let envStatus = {
         node_env: process.env.NODE_ENV || '',
         app_hash: (process.env.RENDER_GIT_COMMIT || '').substring(0, 7),
         database: database.dbInfo(),
     };
-    // assumes db is a mongoose handle
-    // https://mongoosejs.com/docs/api.html#connection_Connection-readyState
-    let dbStatus = database.dbInfo().readyState;
-
-    switch (dbStatus) {
-        case 0:
-            return res.status(500).json({ db_status: 'disconnected', ...envStatus });
-        case 1:
-            return res.status(200).json({ db_status: 'connected', ...envStatus });
-        case 2:
-            return res.status(200).json({ db_status: 'connecting', ...envStatus });
-        case 3:
-            return res.status(500).json({ db_status: 'disconnecting', ...envStatus });
-    }
+    // state 0=disconnected, 1=connected, 2=connecting, 3=disconnecting
+    let readyState = database.dbInfo().readyState;
+    // return 200 for 1,2 and 500 for 0,3
+    let httpStatus = [500, 200, 200, 500][readyState];
+    // http://expressjs.com/en/api.html#res.status
+    return res.status(httpStatus).json(envStatus);
 });
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port} !`));
