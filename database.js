9377a0eaca99cb8d3474eb343fa28a09e4274d14
Chris Kirksey <chris.kirksey@aesbus.com>
2021-08-05 11:47:15 -0500

    Create helper dbInfo

--- database.js
+++ database.js
@@ -2,14 +2,13 @@ const config = require('config'); // https://github.com/lorenwest/node-config/wi
 const dbConfig = config.get('database');
 const mongoose = require('mongoose');
 
-async function connectToDatabase() {
-    // https://docs.mongodb.com/manual/reference/connection-string/#std-label-connections-connection-options
-    // https://mongoosejs.com/docs/connections.html
-    // https://mongoosejs.com/docs/connections.html#multiple_connections
-    // https://stackoverflow.com/questions/22950282/mongoose-schema-vs-model/22950402#22950402
-    // https://masteringjs.io/tutorials/mongoose/mongoose-connect-async
-    // https://attacomsian.com/blog/mongoose-connect-async-await
-    // https://stackoverflow.com/questions/40818016/connect-vs-createconnection
+// https://docs.mongodb.com/manual/reference/connection-string/#std-label-connections-connection-options
+// https://mongoosejs.com/docs/connections.html
+// https://mongoosejs.com/docs/connections.html#multiple_connections
+// https://stackoverflow.com/questions/22950282/mongoose-schema-vs-model/22950402#22950402
+// https://masteringjs.io/tutorials/mongoose/mongoose-connect-async
+// https://attacomsian.com/blog/mongoose-connect-async-await
+// https://stackoverflow.com/questions/40818016/connect-vs-createconnection
 async function dbConnect() {
     try {
         await mongoose.connect(dbConfig.uri, dbConfig.options);
@@ -20,4 +19,13 @@ async function dbConnect() {
         return mongoose; // Let caller handle errors
     }
 }
-exports.connectToDatabase = connectToDatabase;
+exports.dbConnect = dbConnect;
+
+// Helper: takes a connection returned by dbConnect and returns some useful info.
+// https://mongoosejs.com/docs/api/connection.html#connection_Connection-db
+exports.dbInfo = function (db) {
+    db = db || mongoose;
+    let conn = db.connections[0];
+    let { name, host, port, user } = conn;
+    return { name, host, port, user };
+};
