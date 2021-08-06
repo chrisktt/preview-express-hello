20ae6a3b0002b70b6729957539cd352ffe664c6f
Chris Kirksey <chris.kirksey@aesbus.com>
2021-08-06 11:01:30 -0500

    Clean up configuration files production & development

--- config/development.js
+++ config/development.js
@@ -1,25 +1,13 @@
 // https://github.com/lorenwest/node-config/wiki
+
 const config = {
     // Development database
     database: {
         name: 'executiveDashboardDev',
         user: 'mikel.leu',
-        password: 'THIS-IS-A-SECRET',
-        // password: '%40e$bu$17',
         host: 'portal-ssl1052-28.metropcs2017.aesbus.composedb.com:23865,portal-ssl977-29.metropcs2017.aesbus.composedb.com:23865',
         options: {},
     },
 };
-
-// const db = config.database;
-// db.uri = defer(function () {
-//     // must use function() to set value for "this" (it's javascript)
-//     return `mongodb://${db.user}:${db.password}@${db.host}/${db.name}`;
-//     return `mongodb://${this.database.user}:${this.database.password}@${this.database.host}/${this.database.name}`;
-// });
-
-// const db = config.database;
-// db.uri = `mongodb://${db.user}:${db.password}@${db.host}/${db.name}`;
-// console.log('development: setting uri:', db.uri);
 
 module.exports = config;
--- config/production.js
+++ config/production.js
@@ -1,24 +1,12 @@
 // https://github.com/lorenwest/node-config/wiki
-const defer = require('config/defer').deferConfig;
 
 const config = {
     // Production database
     database: {
         name: 'executiveDashboard',
         user: 'subseaAdmin',
-        // password: 'lqWe4*nSd',
-        password: 'SECRET',
         host: 'portal-ssl1052-28.metropcs2017.aesbus.composedb.com:23865,portal-ssl977-29.metropcs2017.aesbus.composedb.com:23865',
-        options: {
-            tls: true,
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            retryWrites: false,
-        },
-        uri: defer(function () {
-            // must use function() to set value for "this" (it's javascript)
-            return `mongodb://${this.database.user}:${this.database.password}@${this.database.host}/${this.database.name}`;
-        }),
+        options: {},
     },
 };
 
